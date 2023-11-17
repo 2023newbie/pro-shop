1afa797b21ce2f926400f8704b53e8fb5eea4ae5
Obar <ducwebdev@gmail.com>
2023-11-17 17:06:20 +0700

    save 20

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
+connectDB() // connect to database
 const port = process.env.PORT || 5000
 
 const app = express()

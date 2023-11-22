b363e1c560eb166d150073ca9c3f2be25029ac23
Obar <ducwebdev@gmail.com>
2023-11-22 16:16:25 +0700

    save 43

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
@@ -10,15 +12,19 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('API is running...')
-})
+// body parser middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// cookie parser middleware
+app.use(cookieParser())
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
 
 app.listen(port, () => {
-  console.log(`Server is running on ${port}`);
+  console.log(`Server is running on ${port}`)
 })

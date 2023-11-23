997ed23f479589daa8142a7686f513c42829d1af
Obar <ducwebdev@gmail.com>
2023-11-23 10:26:47 +0700

    save 46 - auth login, register complete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import User from '../models/userModel.js'
 import asyncHandler from '../middleware/asyncHandler.js'
-import jwt from 'jsonwebtoken'
+import generateToken from '../utils/generateToken.js'
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
@@ -10,19 +10,9 @@ const authUser = asyncHandler(async (req, res, next) => {
   const user = await User.findOne({ email })
 
   if (user && (await user.matchPassword(password))) {
-    const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {
-      expiresIn: '30d'
-    })
+    generateToken(res, user._id)
 
-    // Set JWT as HTTP_Only Cookie
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
-      maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
-    })
-
-    res.json({
+    res.status(200).json({
       _id: user._id,
       name: user.name,
       email: user.email,
@@ -38,28 +28,75 @@ const authUser = asyncHandler(async (req, res, next) => {
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res, next) => {
-  res.send('register user')
+  const { name, email, password } = req.body
+
+  const existUser = await User.findOne({email})
+
+  if (existUser) {
+    res.status(400)
+    throw new Error('User already exists.')
+  }
+
+  const user = await User.create({ name, email, password })
+  
+  if (user) {
+    generateToken(res, user._id)
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data.')
+  }
 })
 
 // @desc    Logout user / clear cookie
 // @route   POST /api/users/logout
 // @access  Private
 const logoutUser = asyncHandler(async (req, res, next) => {
-  res.send('logout user')
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0)
+  })
+
+  res.status(200).json({ message: 'Logged out successfully.'})
 })
 
 // @desc    Get user profile
 // @route   GET /api/users/profile
 // @access  Public
 const getUserProfile = asyncHandler(async (req, res, next) => {
-  res.send('get user profile')
+  res.status(200).json({
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+    isAdmin: req.user.isAdmin,
+  })
 })
 
 // @desc    Update user profile
 // @route   PUT /api/users/profile
 // @access  Private
 const updateUserProfile = asyncHandler(async (req, res, next) => {
-  res.send('update user profile')
+  req.user.name = req.body.name || req.user.name
+  req.user.email = req.body.email || req.user.email
+
+  if (req.body.password) {
+    req.user.password = req.body.password
+  }
+
+  const updatedUser = await req.user.save()
+
+  res.status(200).json({
+    _id: updatedUser._id,
+    name: updatedUser.name,
+    email: updatedUser.email,
+    isAdmin: updatedUser.isAdmin,
+  })
 })
 
 // @desc    Get users

6e86334f03e3857cd7265575dc85ac4948e78a4a
Obar <ducwebdev@gmail.com>
2023-11-25 16:33:09 +0700

    save 55 - checkout/part1/client

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -3,7 +3,7 @@ import { updateCart } from '../utils/cartUtils'
 
 const initialState = localStorage.getItem('cart')
   ? JSON.parse(localStorage.getItem('cart'))
-  : { cartItems: [] }
+  : { cartItems: [], shippingAddress: {}, paymentMethod: 'PayPal' }
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -11,7 +11,7 @@ const cartSlice = createSlice({
   reducers: {
     addToCart: (state, action) => {
       const item = action.payload
-      
+
       const existItem = state.cartItems.find(i => i._id === item._id)
 
       if (existItem) {
@@ -26,11 +26,24 @@ const cartSlice = createSlice({
     },
     removeFromCart: (state, action) => {
       state.cartItems = state.cartItems.filter(i => i._id !== action.payload)
-      
+
       return updateCart(state)
-    }
+    },
+    saveShippingAddress: (state, action) => {
+      state.shippingAddress = action.payload
+      return updateCart(state)
+    },
+    savePaymentMethod: (state, action) => {
+      state.shippingAddress = action.payload
+      return updateCart(state)
+    },
   },
 })
 
-export const { addToCart, removeFromCart } = cartSlice.actions
+export const {
+  addToCart,
+  removeFromCart,
+  saveShippingAddress,
+  savePaymentMethod,
+} = cartSlice.actions
 export default cartSlice.reducer
